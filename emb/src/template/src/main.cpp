64d3bcdef985f3b5ce06edd5e3806cc170c35551
Lisin Dmitriy <d.lisin@thirdpin.ru>
2019-06-04 14:41:40 +0300

    IMPR: Add lib yxml as a non CMake lib example

--- emb/src/template/src/main.cpp
+++ emb/src/template/src/main.cpp
@@ -1,13 +1,18 @@
 #include <libopencm3/stm32/gpio.h>
+#include <yxml/yxml.h>
 
 #include <hw/HwStub.hpp>
+#include <template/version.h>
 
 int main()
 {
     ////
     // Do meaningless stuff for a testing
-    // opencm3 and etl compilation
+    // opencm3, yxml and etl compilation
     ////
+
+    volatile yxml_t just_test;
+    just_test.total = 1;  // never do this
 
     hw::HwStub hw_stub(GPIOA, GPIO1);
 
